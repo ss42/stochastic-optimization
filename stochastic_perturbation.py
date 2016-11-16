a18a56806c2a1634a432c2b4106889d703865430
Brian Hill <brian@lingerhere.org>
2016-11-15 16:18:47 -0800

    Structured the stochastic perturbation to produce (N_M + N_U) * N scalar perturbations all at once.

--- stochastic_perturbation.py
+++ stochastic_perturbation.py
@@ -9,7 +9,7 @@ TOO_SMALL = 0.00001
 
 
 # This does just one number at a time, but it has a guard to guarantee that the number is greater than TOO_SMALL
-def generate_perturbation(sigma):
+def generate_scalar_perturbation(sigma):
     result = 0.0
     while abs(result) < TOO_SMALL:
         result = gauss(0.0, sigma)
@@ -17,13 +17,23 @@ def generate_perturbation(sigma):
     return result
 
 
-# This does a whole tuple.
+# This does a tuple
+def generate_tuple_perturbation(sigma):
+    return tuple([generate_scalar_perturbation(sigma) for _ in range(0, N)])
+
+
+# This does N_M tuples
 def generate_x_perturbation(sigma):
-    # number of components in x is N_M * N
-    return tuple([generate_perturbation(sigma) for _ in range(0, N_M * N)])
+    # number of components in x is N_M
+    return [generate_scalar_perturbation(sigma) for _ in range(0, N_M)]
 
 
-# This does a whole tuple.
+# This does N_U tuples
 def generate_theta_perturbation(sigma):
-    # number of components in theta is N_U * N
-    return tuple([generate_perturbation(sigma) for _ in range(0, N_U * N)])
+    # number of components in theta is N_U
+    return [generate_perturbation(sigma) for _ in range(0, N_U)]
+
+
+# This does a whole perturbation all at once for all of the (N_M + N_U) * N components
+def generate_perturbation(sigma):
+    return generate_x_perturbation(sigma), generate_theta_perturbation(sigma)
