dd47004bc02e6ac7e35a176e14bfa8e056dd586c
Brian Hill <brian@lingerhere.org>
2016-11-15 16:11:14 -0800

    Removed comments suggesting an alternate method from Spall's.

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ from optimization_objective import make_optimization_objective
 
 from random import seed
 
+
 # In simultaneous_perturbation.py, you can implement Spall's algorithm.
 #
-# Alternatively, you can use Ng's method for calculating subsequent guesses as discussed in README.txt
-
-
-# The following is a dummy implementation!! It is your job to implement it. See also the comment above.
+# The following is a dummy implementation!! It is your job to implement it.
 # Really, what this amounts to is implementing Equation 6.4 of Spall.
 # A tricky thing that I don't have much sense of is how the gain vectors that Spall calls a_k and c_k should
 # be chosen.
-
 def optimize_ng_example():
     lamb = 1.0  # I'd call this lambda, except in Python lambda is a keyword. Initialize to 1.0. What should it be?
     optimization_objective_function = make_optimization_objective(lamb, REVIEWS)
